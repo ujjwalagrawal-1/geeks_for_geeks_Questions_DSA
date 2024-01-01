49c7a9c656bd49351c2469f5faf2f15f0ed98569
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-01-01 21:54:09 +0530

    Updated Solution - GfG to GitHub

--- Medium/Painting the Fence/painting-the-fence.cpp
+++ Medium/Painting the Fence/painting-the-fence.cpp
@@ -5,19 +5,43 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution{
+//Back-end complete function template in C++
+
+class Solution
+{
     public:
-    long long countWays(int n, int k){
-        long mod = 1e9 + 7;
-        long same = 0;
-        long diff = k;
-        long total = same + diff;
-        for(int i = 2; i <= n; ++i) {
-            same = diff * 1;
-            diff = (total * (k - 1)) % mod;
-            total = same + diff;
+    //Function to count the number of ways.
+    long long countWays(int n, int k)
+    {
+        //Creating an array to store the number of ways for each number of fences.
+        long long dp[n + 1];
+
+        //Initializing the dp array with 0.
+        memset(dp, 0, sizeof(dp));
+
+        //Setting the base case, when there is only 1 fence.
+        dp[1] = k;
+
+        //Initializing variables to keep count of same and different color fences.
+        long long same = 0, diff = k;
+
+        //Iterating over the fences starting from 2.
+        for (int i = 2; i <= n; i++)
+        {
+            //Updating the value of same to the previous value of diff.
+            same = diff;
+
+            //Calculating the number of ways for different color fences.
+            diff = dp[i-1] * (k-1);
+            diff%=1000000007;
+
+            //Calculating the total number of ways.
+            dp[i] = (same + diff);
+            dp[i]%=1000000007;
         }
-        return total % mod;
+
+        //Returning the number of ways for n fences.
+        return dp[n];
     }
 };
 
